6c2fb34d1c0de67f092bcdf4a7553d5e8505a663
EDA <eda@mac-mini-6.local>
2016-08-05 16:02:41 +1200

    working

--- public/soundscript.js
+++ public/soundscript.js
@@ -1,6 +1,9 @@
-$('.sound').on('click',function() {
+$('.sound').on('click',function(evt) {
  var file = $(this).data('soundfile')
  var sound = new Audio()
+ var image = $(this).data('image')
+ console.log(image)
+ $(evt.target).css('background-image', 'url(midi8.png)')
  sound.src = file
  sound.play()
 })
